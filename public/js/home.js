c634580cf25608024278aa184e51383f7fdbe5b7
Vincent Zhang <vzhang@purdue.edu>
2016-04-23 21:34:57 -0400

    Fix socket interruption

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,13 @@
 var socket = io();
+$(window).on('beforeunload', function(){
+	console.log("unload");
+    socket.close();
+});
 socket.on('connection', function(){
     console.log('user');
 });
-$(window).on('beforeunload', function(){
-    socket.close();
-});
-
 function newRoom() {
 	console.log("New room request");
 }
+
+
