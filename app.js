a3e19e5e67273cd5eb26507e392dce0d0d8209bb
Vincent Zhang <vzhang@purdue.edu>
2016-04-23 22:22:30 -0400

    Create/join room works, shows play area

--- app.js
+++ app.js
@@ -33,92 +33,85 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public');
 
 app.use(lessMiddleware({
-  src: __dirname + "/public",
-  compress: true
+    src: __dirname + "/public",
+    compress: true
 }));
 app.use(express.static(__dirname + '/public'));
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
-   res.render('index');
+    res.render('index');
+});
+
+app.get('/room/:roomId', function(req, res) {
+    var roomId = req.params.roomId;
+    res.render('index');
 });
 
 function getNewRoom(){
-	//need to return the starting state of a room
-	return 0;
+    //need to return the starting state of a room
+    return 0;
 }
 
 function initRoom(roomID){
-	
-	var data = getNewRoom();
-	//redisclient.hmset(roomID,{'data': data,'numofplayers':0});
-	
-	//initialze redis information for the room
+    var data = getNewRoom();
+    //redisclient.hmset(roomID,{'data': data,'numofplayers':0});
+
+    //initialze redis information for the room
 }
 
 var server = app.listen(3000, function () {
-  console.log('Example app listening on port 3000!'  );
+    console.log('Example app listening on port 3000!'  );
 });
 
 io.listen(server);
 
 io.sockets.on('connection', function(socket) {
-	
-  console.log("Connected");
+    console.log("Connected");
 
-  //request to join a room
-  socket.on('join', function(channel) {
-    socket.get('room', function() {
-    	
-      socket.set('room', channel, function() {
-        socket.join(channel);
-      });
-      
-    });
-  });
-  
-  
-  //request to create a new room
-  
-   socket.on('create', function() {
-	  var roomid = uuid.v4();
-	  initRoom(roomid); 
-      socket.set('room', roomid, function() {
+    //request to join a room
+    socket.on('join', function(param) {
+        var roomid = param.roomId;
         socket.join(roomid);
-      });
+        console.log('Joined room ' + roomid);
+        socket.emit('joinroom', {roomId: roomid});
+    });
 
-  });
-  
-  
-  
-  //request to send a message to the room
-   socket.on('message', function(msg,user) {
-    socket.get('room', function(err, room) {
-      if (err) {
-        socket.emit('error', err);
-      } else if (room) {
-        socket.broadcast.to(room).emit('broadcast_message', msg,user);
-      } else {
-        socket.emit('error', 'no room');
-      }
+    //request to create a new room
+    socket.on('create', function() {
+        console.log('creating room...');
+        var roomid = uuid.v4();
+        initRoom(roomid); 
+        socket.join(roomid);
+        console.log('Created room ' + roomid);
+        socket.emit('createroom', {roomId: roomid});
+        socket.emit('joinroom', {roomId: roomid});
     });
-  });
-  
-  
-  
-  
-  //request to update a room state 
-  socket.on('new_action', function(msg,user) {
-    socket.get('channel', function(err, room) {
-      if (err) {
-        socket.emit('error', err);
-      } else if (room) {
-        socket.broadcast.to(room).emit('broadcast', msg,user);
-      } else {
-        socket.emit('error', 'no room');
-      }
+
+    //request to send a message to the room
+    socket.on('message', function(msg,user) {
+        socket.get('room', function(err, room) {
+            if (err) {
+                socket.emit('error', err);
+            } else if (room) {
+                socket.broadcast.to(room).emit('broadcast_message', msg,user);
+            } else {
+                socket.emit('error', 'no room');
+            }
+        });
     });
-  });
-  
-  
+
+    //request to update a room state 
+    socket.on('new_action', function(msg,user) {
+        socket.get('channel', function(err, room) {
+            if (err) {
+                socket.emit('error', err);
+            } else if (room) {
+                socket.broadcast.to(room).emit('broadcast', msg,user);
+            } else {
+                socket.emit('error', 'no room');
+            }
+        });
+    });
+
 });
--- public/js/home.js
+++ public/js/home.js
@@ -1,13 +1,42 @@
 var socket = io();
 $(window).on('beforeunload', function(){
-	console.log("unload");
+	console.log("Unloading");
     socket.close();
 });
+
 socket.on('connection', function(){
-    console.log('user');
+    console.log('Connected');
 });
+
+socket.on('createroom', function(param) {
+	console.log("Created room " + param.roomId);
+	history.pushState({}, 'MultiSand', 'room/' + param.roomId);
+});
+
+socket.on('joinroom', function(param) {
+	console.log("Joined room " + param.roomId);
+	showPlayArea();
+});
+
 function newRoom() {
 	console.log("New room request");
+	socket.emit('create', {});
 }
 
+var index = window.location.href.indexOf('/room/');
+if (index > -1) {
+	$('#newroom').hide();
+	var joinRm = window.location.href.substring(index + 6);
+	console.log("Join room " + joinRm);
+	socket.emit('join', {roomId: joinRm});
+}
+
+function showPlayArea() {
+	$('#newroom').hide();
+	$('#play').show();
+	var s = document.createElement("script");
+	s.type = "text/javascript";
+	s.src = "/js/game.js";
+	$("body").append(s);
+}
 
