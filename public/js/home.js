719e1955917534b3e29984911fb1afbfaa74a2c5
Vincent Zhang <vzhang@purdue.edu>
2016-04-24 23:08:12 -0400

    Add chat area

--- public/js/home.js
+++ public/js/home.js
@@ -4,10 +4,6 @@ $(window).on('beforeunload', function(){
     socket.close();
 });
 
-socket.on('connection', function(){
-    console.log('Connected');
-});
-
 socket.on('createroom', function(param) {
 	console.log("Created room " + param.roomId);
 	history.pushState({}, 'MultiSand', 'room/' + param.roomId);
@@ -18,17 +14,17 @@ socket.on('joinroom', function(param) {
 	showPlayArea();
 });
 
-function newRoom() {
-	console.log("New room request");
-	socket.emit('create', {});
-}
-
 var index = window.location.href.indexOf('/room/');
 if (index > -1) {
 	$('#newroom').hide();
 	var joinRm = window.location.href.substring(index + 6);
 	console.log("Join room " + joinRm);
 	socket.emit('join', {roomId: joinRm});
+}
+
+function newRoom() {
+	console.log("New room request");
+	socket.emit('create', {});
 }
 
 function showPlayArea() {
@@ -40,3 +36,8 @@ function showPlayArea() {
 	$("body").append(s);
 }
 
+$('#chat').submit(function(){
+	socket.emit('chat_msg', $('#m').val());
+	$('#m').val('');
+	return false;
+});
